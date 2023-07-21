28159bf168af9852bd715baf49cc598fd64b5704
sqiu4 <sqiu4@vols.utk.edu>
2023-07-21 15:33:06 -0400

    minor fix

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 # Preliminaries
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install python3-pip
+sudo apt-get install python3-pip -y
 wget https://bootstrap.pypa.io/pip/3.6/get-pip.py
 python3 get-pip.py
 
 
 # jetson-inference installation (also installs PyTorch)
 sudo apt-get update
-sudo apt-get install git cmake libpython3-dev python3-numpy
+sudo apt-get install git cmake libpython3-dev python3-numpy -y
 git clone --recursive --depth=1 https://github.com/dusty-nv/jetson-inference
 cd jetson-inference
 mkdir build
