71668e9cfc9ee199f461fa339e791be50a9db890
sqiu4 <sqiu4@vols.utk.edu>
2023-07-30 15:32:14 -0400

    tf install script

---
+++ tf_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran
+
+sudo apt-get install python3-pip
+Sudo pip3 install -U pip
+sudo pip3 install -U pip testresources setuptools==49.6.0 
+sudo pip3 install -U pip testresources setuptools numpy==1.16.1 future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
+sudo pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v45 tensorflow==2.3.1
